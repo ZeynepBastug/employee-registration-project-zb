43610d77e2ce0fc2122e5271f81cf60c8e02b3a4
ZB <zeynepbstg@hotmail.com>
2023-03-30 14:26:18 -0400

    project created

---
+++ src/test/java/com/example/employeeregistrationprojectzb/EmployeeRegistrationProjectZbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeregistrationprojectzb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationProjectZbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
