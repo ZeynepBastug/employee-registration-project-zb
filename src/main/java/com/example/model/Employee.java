0468dd3a6e9903355f25ed43f5c766ad21474019
ZB <zeynepbstg@hotmail.com>
2023-03-30 20:58:22 -0400

    validation done

--- src/main/java/com/example/model/Employee.java
+++ src/main/java/com/example/model/Employee.java
@@ -3,6 +3,10 @@ package com.example.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -12,6 +16,12 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+   // @NotNull -> Except null, everything is accepted
+   // @NotEmpty -> Except null and empty (""), everything is accepted
+   // @NotBlank -> Except null, empty(""), blanc(" ") is accepted
+
+    @NotBlank
+    @Size(max= 12, min=2)
     private String firstName;
     private String lastName;
 
